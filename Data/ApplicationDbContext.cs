5d94fbf690cbe6e3198ec48b95a44f6a4528a91e
Danah Alsayed <danahba03@gmail.com>
2024-09-25 15:34:29 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Session6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session6.Data
+{
+    internal class ApplicationDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = . ; database = Asp9_EF ; Trusted_Connection = true;TrustServerCertificate = True") ;
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
